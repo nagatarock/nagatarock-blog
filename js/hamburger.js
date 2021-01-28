5b9285495e44fd791182fa15557ae32db028fe1d
永田健作 <>
2021-01-28 22:33:40 +0900

    ハンバーガーメニューの実装

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,7 +2,9 @@ jQuery( function( $ ) {
 	$( ".p-header_menu_btn" ).on( "click", function() {
 		$( this ).toggleClass( "is-open" );
 		$( ".p-header_menu_menu" ).toggleClass( "is-open" );
-		$( ".menu-bg" ).toggleClass( "is-open" );
-		$( "body" ).toggleClass( "is-open" );
+		$( ".p-header_menu_btn_background" ).toggleClass( "is-open" );
+		$( "header" ).toggleClass( "is-open" );
+		$( ".c-main_container_menu" ).toggleClass( "is-open" );
+		$( ".c-main_container_menu_menu" ).toggleClass( "is-open" );
 	} );
 } );
