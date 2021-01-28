0c656f2a6c99112412f7f73aaf8e161cc233b417
永田健作 <>
2021-01-29 01:55:49 +0900

    トップ画面の写真にアニメーションを。ランダムに写真を表示 ヘッダーメニューにアンダーバーのアニメーションを追加

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,4 @@
+//ハンバーガーメニュー用//
 jQuery( function( $ ) {
 	$( ".p-header_menu_btn" ).on( "click", function() {
 		$( this ).toggleClass( "is-open" );
@@ -8,3 +9,16 @@ jQuery( function( $ ) {
 		$( ".c-main_container_menu_menu" ).toggleClass( "is-open" );
 	} );
 } );
+
+//トップ写真をランダム表示//
+$(function(){
+var array =[
+		"/img/top1.JPG",
+		"/img/top2.JPG",
+		"/img/top3.jpeg"
+];
+var l = array.length;
+var r = Math.floor(Math.random()*l);
+var imgurl = array[r];
+$("img#random").attr({"src":imgurl});
+});
