5bc10a29baa49113f31b2c44b06ab4af2cbc0242
永田健作 <nagatarock2@gmail.com>
2021-06-22 00:08:00 +0900

    ポートフォリオゾーンのコーディング

--- front-page.php
+++ front-page.php
@@ -7,6 +7,7 @@
     </div>
     <!--p-main_container-->
 </section>
+<!-- プロフィールページ -->
 <section class="p-profile">
     <hr>
     <div class="c-container_title">
@@ -50,30 +51,65 @@
             </div>
             <div class="c-skills__card">
                 <div class="skills__image">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/wordpress-icon.svg" alt="ワードプレスのイメージ図">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/wordpress-icon.svg" alt="ワードプレスのイメージ図">
                 </div>
             </div>
             <div class="c-skills__card">
                 <div class="skills__image">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/github-icon.svg" alt="ギットハブのイメージ図">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/github-icon.svg" alt="ギットハブのイメージ図">
                 </div>
             </div>
             <div class="c-skills__card">
                 <div class="skills__image">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/ai.svg" alt="イラストレーターのイメージ図">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/ai.svg" alt="イラストレーターのイメージ図">
                 </div>
             </div>
             <div class="c-skills__card">
                 <div class="skills__image">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/xd.svg" alt="エックスディーのイメージ図">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/xd.svg" alt="エックスディーのイメージ図">
                 </div>
             </div>
             <div class="c-skills__card">
                 <div class="skills__image">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/lstep.svg" alt="得るステップのイメージ図">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/lstep.svg" alt="得るステップのイメージ図">
                 </div>
             </div>
         </div>
+    </div>
+</section>
+<!-- ポートフォリオページ -->
+<section class="p-portfolio">
+<hr>
+    <div class="c-container_title">
+        <h2>PORTFOLIO
+        </h2>
+    </div>
+    <div class="p-portfolio__box">
+        <?php
+        $args = [
+            'post_type' => 'site', // カスタム投稿名が「site」の場合
+            'posts_per_page' => 10, // 表示する数
+        ];
+        $my_query = new WP_Query($args); ?>
+
+        <?php if ($my_query->have_posts()) : ?>
+            <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+                <!-- ▽ ループ開始 ▽ -->
+                    <div class="p-portfolio__box__thumb">
+                    <a href="<?php the_permalink(); ?>">
+                    <div class="p-portfolio__box__thumb__mask">
+                    <img src="<?php the_post_thumbnail('full',array("alt" => get_the_title()));?>
+                    </div>
+                    <div class="thumb__title">
+                    <h3><?php the_title(); ?></h3>
+                    </div>
+                    </a>
+                    </div>
+            <?php endwhile; ?>
+        <?php else :?>
+            <p>まだ投稿がありません。</p>
+        <?php endif;
+        wp_reset_postdata(); ?>
     </div>
 </section>
 <hr>
