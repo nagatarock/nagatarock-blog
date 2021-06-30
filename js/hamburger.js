f8ed671df156fb0565184d8b17c518c48be6a3cb
永田健作 <nagatarock2@gmail.com>
2021-06-30 13:19:03 +0900

    ハンバーガーメニューのページ内リンクでジャンプしたあと、自動的にハンバーガーメニューが閉じるように設定

--- js/hamburger.js
+++ js/hamburger.js
@@ -8,4 +8,12 @@ jQuery( function( $ ) {
 		$( ".c-hamburger_menu" ).toggleClass( "is-open" );
 		$( ".c-hamburger_menu_menu" ).toggleClass( "is-open" );
 	} );
+	$(".c-hamburger_menu ul  li  a").on("click", function() {
+		$(".c-hamburger_menu").removeClass("is-open");
+		$(".c-hamburger_menu_menu").removeClass("is-open");
+		$(".p-header_menu_btn").removeClass("is-open");
+		$(".p-header_menu_menu").removeClass("is-open");
+		$(".p-header_menu_btn_background").removeClass("is-open");
+		$("header").removeClass("is-open");
+	});
 } );
